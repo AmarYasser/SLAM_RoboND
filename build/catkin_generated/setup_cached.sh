a182ea5e3f3d9d368e50a29d28e8e9e0f6f1fd4e
Ammar Yasser <amaryaser46@gmail.com>
2020-05-13 16:50:48 -0700

    SLAM using RTAP-Map

---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/asd/Downloads/SLAM/devel:$CMAKE_PREFIX_PATH"
+export PWD="/home/asd/Downloads/SLAM/build"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/asd/Downloads/SLAM/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/asd/Downloads/SLAM/src:$ROS_PACKAGE_PATH"
---
+++ build/slam_project/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/asd/Downloads/SLAM/src/slam_project
+# Build directory: /home/asd/Downloads/SLAM/build/slam_project
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
